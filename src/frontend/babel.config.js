7db3ad45f6bd01d59f140f6bb43561ebfb08f7c6
kevinomaha <kevin@corysite.com>
2025-03-21 14:16:33 -0500

    Fix deprecated dependencies in frontend package.json

---
+++ src/frontend/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+  plugins: [
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-nullish-coalescing-operator',
+    '@babel/plugin-transform-numeric-separator',
+    '@babel/plugin-transform-optional-chaining',
+    '@babel/plugin-transform-private-methods',
+    '@babel/plugin-transform-private-property-in-object'
+  ]
+};
